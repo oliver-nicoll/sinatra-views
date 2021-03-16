315d571f18babae462cd6ca8a0470758e9d281bb
Nicoll Oliver <nicolldon96@gmail.com>
2021-03-15 23:44:18 -0400

    commit tree 4ba92a24c579ac3faafd868d4375c786e9df0cb5

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World</h1>
+		This HTML code is inside of a '.erb' file"
 	end
+
+	get '/' do
+		erb :index
+	  end
+  
+	get "/info" do
+		erb :info
+	end 
 end
